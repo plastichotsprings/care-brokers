229f6ae6a9d78ea1d81ff193ccfab37fcf7e5cc9
ozzmotic <ozzmotic@gmail.com>
2021-09-13 11:01:50 -0400

    Start adding content to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>A 'care broker' is a company, organization, or individual acting as an intermediary between care workers and their clients. This site collects the anonymized words and experiences of care workers who navigate care brokers in their day-to-day lives. We hope to show the similarities and differences in how care brokers structure and control caregivers and their workplaces.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,8 +18,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/page-2/">About the authors</Link>
     </p>
   </Layout>
 )
