eba78c0022ff477388c3e93c4002ff9868737262
ozzmotic <ozzmotic@gmail.com>
2021-09-14 08:24:34 -0400

    Add unstyled react-slick slider and placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const sliderSettings = {
+  autoplay: false,
+  arrows: true,
+  dots: false,
+  infinite: false
+}
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <p>A 'care broker' is a company, organization, or individual acting as an intermediary between care workers and their clients. This site collects the anonymized words and experiences of care workers who navigate care brokers in their day-to-day lives. We hope to show the similarities and differences in how care brokers structure and control caregivers and their workplaces.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    
+    <Slider {...sliderSettings}>
+      <div>
+        Some text
+      </div>
+      <div>
+        Some more text
+      </div>
+    </Slider>
+
     <p>
       <Link to="/page-2/">About the authors</Link>
     </p>
