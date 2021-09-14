83d08fc3831e3d2d3a408f661f38e9fa87da75d1
ozzmotic <ozzmotic@gmail.com>
2021-09-14 09:17:19 -0400

    Import styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { graphql } from "gatsby"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -20,6 +19,27 @@ const IndexPage = () => (
     <Seo title="Home" />
     <p>A 'care broker' is a company, organization, or individual acting as an intermediary between care workers and their clients. This site collects the anonymized words and experiences of care workers who navigate care brokers in their day-to-day lives. We hope to show the similarities and differences in how care brokers structure and control caregivers and their workplaces.</p>
     
+    <h2>Agencies</h2>
+    <Slider {...sliderSettings}>
+      <div>
+        Some text
+      </div>
+      <div>
+        Some more text
+      </div>
+    </Slider>
+
+    <h2>Care.com</h2>
+    <Slider {...sliderSettings}>
+      <div>
+        Some text
+      </div>
+      <div>
+        Some more text
+      </div>
+    </Slider>
+
+    <h2>Co-operatives</h2>
     <Slider {...sliderSettings}>
       <div>
         Some text
