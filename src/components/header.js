2c20ca46d3b7216f30aa926e09c46b8cbb7fe3a8
6frz82g <freesoul@u.northwestern.edu>
2021-09-27 12:50:38 -0400

    header graphic

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,15 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
+export function Headergraphic() {
+  return (
+    <StaticImage
+    src="../images/headergraphic.png"
+    />
+  )
+}
 
 const Header = ({ siteTitle }) => (
   <header
@@ -12,7 +21,7 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        // maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
@@ -27,6 +36,30 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+    </div>
+    <div
+      style={{
+        marginLeft: `2em`,
+        marginRight: `2em`,
+      }}
+      >
+      <h3
+        style={{
+          color: `white`,
+         }}
+       >
+          Care Broker (n:) a company, organization, or individual acting as an intermediary between care workers and their clients.
+      </h3>
+    </div>
+    <div>
+      <center>
+        <StaticImage
+        style= {{
+          margin: 0,
+        }}
+        src="../images/headergraphic.png"
+        />
+      </center>
     </div>
   </header>
 )
