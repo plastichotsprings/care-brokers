18a111858afb66bf1c43b7018e306e222cefdf6d
ozzmotic <ozzmotic@gmail.com>
2021-09-29 14:26:37 -0400

    Working mobile navbar!

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -19,6 +19,22 @@ const MobileNavMenu = () => {
           <div></div>
           <div></div>
         </MenuIcon>
+        <MenuLinks menuOpen={menuOpen}>
+          <ul>
+            <li>
+              <AnchorLink to="/#who" title="Who are care brokers?">Who?</AnchorLink>
+            </li> 
+            <li>
+              <AnchorLink to="/#findings" title="Our findings">Findings</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink to="/#culture" title="Challenging the Dominant Culture">Challenging the Culture</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink to="/#about" title="About the study and authors">About</AnchorLink>
+            </li>
+          </ul>
+        </MenuLinks>
       </MenuIconContainer>
   </MenuBar>
   )
@@ -83,6 +99,7 @@ const MenuIcon = styled.button`
     background: ${({ menuOpen }) => (menuOpen ? "red" : "black")};
     border-radius: 10px;
     transform-origin: 1px;
+    transition: opacity 300ms, transform 300ms;
 
     :first-child {
       transform: ${({ menuOpen }) => (menuOpen ? "rotate(40deg)" : "rotate(0)")};
@@ -97,4 +114,40 @@ const MenuIcon = styled.button`
       transform: ${ ( { menuOpen }) => menuOpen ? "rotate(-40deg)" : "rotate(0)"}    
     }
   }
+`;
+
+const MenuLinks = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  background: #E7CAF8;
+  position: absolute;
+  z-index: 5;
+  top: 0;
+  right: 0;
+  width: 30%;
+  transition: transform 300ms;
+  transform: ${({ menuOpen }) => menuOpen ? "translateX(0)" : "translateX(100%)"};
+
+  ul {
+    margin: 0;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+    display: flex;
+    border-style: dotted;
+    background: #e0bcf6;
+
+    :first-child {
+      margin-top: 88px;
+    }
+
+    li {
+      list-style: none;
+      padding: 0px 5px 0px 5px;
+      text-align: center;
+    }
+  }
+
 `;
