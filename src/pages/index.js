8e2f2091be2e52c4cf827e7aac9c6e134ada5b79
6frz82g <freesoul@u.northwestern.edu>
2021-09-30 20:27:54 -0400

    reading list

--- src/pages/index.js
+++ src/pages/index.js
@@ -191,7 +191,7 @@ export default function IndexPage({ data }) {
       </SectionFindings>
       <br />
       <SectionSoWhat>
-      <h1>So What?</h1>
+      <h1>So What Now?</h1>
       <h3>Summary sentence...</h3>
       <p style={{fontWeight:`bold`,}}>
         <ul>
@@ -237,18 +237,134 @@ export default function IndexPage({ data }) {
         <h4>
         Further Reading
         </h4>
-        <ul>
-          <li class="readinglist">
+        <ul class="readinglist">
+          <li>
             <a href="https://journals.sagepub.com/doi/pdf/10.1177/0141778919879744"
             target="_blank" style={{textDecoration:`none`, color:`black`,}}>
             Amrute, Sareeta. “Of Techno-Ethics and Techno-Affects.” Feminist Review, vol. 123, no. 1, 2019, pp. 56–73. Crossref, doi:10.1177/0141778919879744.
             </a>
           </li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a href="https://www.sup.org/books/title/?id=17330"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Boris, Eileen and Parreñas, Rhacel. Intimate Labors: Cultures, Technologies, and the Politics of Care. Palo Alto: Stanford University Press, 2010.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.tandfonline.com/doi/abs/10.1080/14616742.2015.1007728?journalCode=rfjp20"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Brown, Rachel. “Re-examining the Transnational Nanny.” International Feminist Journal of Politics 18:2, 210-229, 2015.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.google.com/books/edition/Disposable_Domestics/QdxeDAAAQBAJ?hl=en&gbpv=0&kptab=overview"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Chang, Grace. Disposable Domestics: Immigrant Women Workers in the Global Economy. Chicago: Haymarket Books, 2016.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.google.com/books/edition/Riding_for_Deliveroo/bq5rwwEACAAJ?hl=en"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Cant, Callum. Riding for Deliveroo: Resistance in the New Economy. Malden: Polity Press, 2020.
+            </a>
+          </li>
+          <li>
+            <a href=""
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Carrillo, Teresa. “The Best of Care: Latinas as Transnational Mothers and Caregivers” in Technofuturos: Critical Interventions in Latina/o Studies. Minneapolis: Lexington Books, 2007.
+            </a>
+          </li>
+          <li>
+            <a href="https://books.google.com/books/about/Gender_and_Technology_in_the_Making.html?id=85mwBllqv98C"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Cockburn, Cynthia and Susan Ormrod. Gender and Technology in the Making. SAGE Publications, 1993.
+            </a>
+          </li>
+          <li>
+            <a href=""
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Davis, Angela. “The Approaching Obsolescence of Housework” in Women, Race and Class. New York: Random House (Vintage Books), 1983.
+            </a>
+          </li>
+          <li>
+            <a href="https://books.google.com/books?id=WEw9CwAAQBAJ&dq=Dyer-Witheford,+Nick.+Cyber-proletariat:+Global+Labor+in+the+Digital+Vortex.+London:+Pluto+Press,+2015.&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwj3go3e7KfzAhX3MlkFHUmTB1cQ6AF6BAgDEAI"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Dyer-Witheford, Nick. Cyber-proletariat: Global Labor in the Digital Vortex. London: Pluto Press, 2015.
+            </a>
+          </li>
+          <li>
+            <a href="https://politybooks.com/bookdetail/?isbn=9781509520626&subject_id=88"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Hester, Helen. Xenofeminism. Malden: Polity, 2018.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.ucpress.edu/book/9780520251717/domestica"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Hondagneu-Sotelo, Pierrette. Domestica: Immigrant Workers Cleaning and Caring in the Shadows of Affluence. Los Angeles: University of California Press, 2001.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.jstor.org/stable/j.ctt1287j8b"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Huws, Ursula. Labor in the Global Digital Economy: The Cybertariat Comes of Age. Monthly Review Press, 2014.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.ilo.org/travail/Whatsnew/WCMS_173363/lang--en/index.htm"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            International Labor Office. Domestic Workers Around the World: Global and Regional Statistics and the Extent of Legal Protections. Geneva: International Labor Organization, 2013.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.sup.org/books/title/?id=1432"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Matthews, Glenna. Silicon Valley, Women and the California Dream: Gender, Class and Opportunity in the Twentieth Century. Palo Alto: Stanford University Press,
+            </a>
+          </li>
+          <li>
+            <a href="https://books.google.com/books/about/Breaking_Things_at_Work.html?id=vRMbywEACAAJ"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Mueller, Gavin. Breaking Things at Work: The Luddites Were Right About Why You Hate Your Job. New York: Verso, 2021.
+            </a>
+          </li>
+          <li>
+            <a href="https://nyupress.org/9781479837243/algorithms-of-oppression/"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Noble, Safiya. Algorithms of Oppression: How Search Engines Reinforce Racism. New York: New York University Press, 2018.
+            </a>
+          </li>
+          <li>
+            <a href="https://onlinelibrary.wiley.com/doi/book/10.1002/9780470696569"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Ong, Aihwa and Stephen J. Collier. Global Assemblages: Technology, Politics, and Ethics as Anthropological Problems. Blackwell Publishing, 2005.
+            </a>
+          </li>
+          <li>
+            <a href="https://archive.org/details/betweenwomendome00roll"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Rollins, Judith. Between Women: Domestics and their Employers. Philadelphia: Temple University Press, 1985.
+            </a>
+          </li>
+          <li>
+            <a href="https://books.google.com/books/about/More_Work_For_Mother.html?id=9YM1tAEACAAJ"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Schwartz Cowan, Ruth. More Work for Mother: The Ironies of Household Technology from the Open Hearth to the Microwave. Basic Books, 1985.
+            </a>
+          </li>
+          <li>
+            <a href="https://www.wiley.com/en-us/Platform+Capitalism-p-9781509504862"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Srnicek, Nick. Platform Capitalism. Malden, MA: Polity Press, 2017.
+            </a>
+          </li>
+          <li>
+            <a href="https://datasociety.net/wp-content/uploads/2018/06/Data_Society_Beyond_Disruption_FINAL.pdf"
+            target="_blank" style={{textDecoration:`none`, color:`black`,}}>
+            Ticona,  Julia, Alexandra Mateescu and Alex Rosenblat. Beyond Disruption: How Tech Shapes Labor Across Domestic Work and Ridehailing. New York, NY: Data and Society, 2018.
+            </a>
+          </li>
+
         </ul>
       </SectionCulture>
       <br />
@@ -258,7 +374,7 @@ export default function IndexPage({ data }) {
 
       <p>Magally "Maga" Miranda Alcázar (she/they) is a graduate student in Chicana/o and Central American Studies at UCLA. Using methods that emphasize the co-production of knowledge with rank-and-file workers, their research explores the contested meanings of care, work and Latinidad in the context of a globalized economy of care and information. Maga is also the co-founder of the multimedia platform SAL(T): Xicana Marxist Thoughts.</p>
 
-      <p>Freesoul El Shabazz-Thompson (he/him) is a designer, researcher, and artist exploring community-based creation and control of technologies. He is interested in democratic design and the social role of design in developing artifacts and experiences that challenge unsustainable and inequitable practices and beliefs.</p>
+      <p>Freesoul El Shabazz-Thompson (he/him) is a designer, researcher, and artist exploring communal creation and control of technologies. He is focused on democratic design and the social role of design in developing artifacts and experiences that challenge unsustainable and inequitable practices and beliefs. He is currently a research associate in design and HCI with the <a href="https://www.thecoalalab.com/" target="_blank">CoALA Lab</a> led by Dr. Ken Holstein at Carnegie Mellon University.</p>
 
       <p>Alex Ahmed (she/her) is a postdoc at Carnegie Mellon University, affiliated with the Tech Solidarity Lab led by Dr. Sarah Fox. Her research applies a Marxist, worker-centered perspective on the “future of work,” participatory design, and the intersections between labor and technology. Her writing has been published in several academic venues, in addition to Pangyrus Literary Magazine and a forthcoming piece in Science for the People Magazine.</p>
       </SectionIntro>
